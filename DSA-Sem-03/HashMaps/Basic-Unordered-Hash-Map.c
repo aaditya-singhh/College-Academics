70cb7b2048c554e6df58d15861b416f24d4dc6fc
Saksham Shrey <sakshamshrey_work@icloud.com>
2023-09-03 12:11:55 +0530

    Update Basic-Unordered-Hash-Map.c

--- DSA-Sem-03/HashMaps/Basic-Unordered-Hash-Map.c
+++ DSA-Sem-03/HashMaps/Basic-Unordered-Hash-Map.c
@@ -1,3 +1,51 @@
+/*
+
+
+
+Design a data structure that supports the following operations in O(1) time.
+insert(x): Inserts an item x to the data structure if not already present.
+remove(x): Removes item x from the data structure if present.
+search(x): Searches an item x in the data structure.
+getRandom(): Returns a random element from current set of elements
+
+Following are detailed operations.
+ insert(x) 
+
+1. Check if x is already present by doing a hash map lookup. 
+2. If not present, then insert it at the end of the array. 
+3. Add in the hash table also, x is added as key and last array
+index as the index.
+
+ remove(x) 
+
+1. Check if x is present by doing a hash map lookup. 
+2. If present, then find its index and remove it from a hash
+map. 
+3. Swap the last element with this element in an array and
+remove the last element. 
+Swapping is done because the last element can be removed
+in O(1) time. 
+4. Update index of the last element in a hash map.
+
+ getRandom() 
+
+1. Generate a random number from 0 to last index. 
+2. Return the array element at the randomly generated index.
+
+ search(x) 
+
+ Do a lookup for x in hash map.
+
+
+
+
+*/
+
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
